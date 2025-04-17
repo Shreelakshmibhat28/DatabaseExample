70678494645f5ee36a0d964657df7825c8e63d57
Shreelakshmibhat28 <kshreebhat28@gmail.com>
2025-04-17 17:49:47 +0530

    SP 1

--- app/src/main/java/com/example/database_example/HomeActivity.java
+++ app/src/main/java/com/example/database_example/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.example.database_example;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +17,22 @@ import androidx.core.view.WindowInsetsCompat;
 public class HomeActivity extends AppCompatActivity {
 
     Button btnLogout;
+    TextView uName, uEmail;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         btnLogout = findViewById(R.id.btnLogout);
+        uName = findViewById(R.id.uName);
+        uEmail = findViewById(R.id.uEmail);
+
+        String uname = getIntent().getStringExtra("uname");
+        String email = getIntent().getStringExtra("email");
+
+        uName.setText("Welcome, "+uname + "!");
+        uEmail.setText("Your email: "+email);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
